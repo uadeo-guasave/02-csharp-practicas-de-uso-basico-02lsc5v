dc743558d9c96701e905d556be727a074233714f
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-09-04 16:03:43 -0600

    05. Prácticas de C#
    Modelo de datos representado en C#.
    TODO: Realizar las clases de las tablas pendientes.

---
+++ Db/ddl.sql
@@ -0,0 +1,31 @@
+-- Data Definition Language Sqlite
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(16) NOT NULL UNIQUE,
+  description VARCHAR(200) NOT NULL,
+  level INTEGER NOT NULL
+);
+
+CREATE TABLE permissions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(16) NOT NULL UNIQUE,
+  description VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE roles_has_permissions (
+  role_id INTEGER NOT NULL,
+  permission_id INTEGER NOT NULL,
+  PRIMARY KEY (role_id, permission_id)
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(16) NOT NULL UNIQUE,
+  password VARCHAR(200) NOT NULL,
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  email VARCHAR(200) NOT NULL UNIQUE,
+  remember_token VARCHAR(200) DEFAULT NULL,
+  gender INTEGER NOT NULL,
+  role_id INTEGER NOT NULL
+);
--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace _02_csharp_primeros_pasos.Models
 {
     public enum gender {
@@ -6,13 +8,30 @@ namespace _02_csharp_primeros_pasos.Models
     public class User
     {
         public int Id { get; set; }
+        
+        [Required, MaxLength(16)]
         public string Name { get; set; }
+
+        [Required, MaxLength(200)]
         public string Password { get; set; }
+
+        [Required, MaxLength(50)]
         public string Firstname { get; set; }
+
+        [Required, MaxLength(50)]
         public string Lastname { get; set; }
+
+        [Required, MaxLength(200)]
         public string Email { get; set; }
+
+        [MaxLength(200)]
         public string RememberToken { get; set; }
+
+        [Required]
         public gender Gender { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
 
     }
 }
